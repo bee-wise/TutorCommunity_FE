85974bfe84a4719ba0db55d73db2524361d88c84
Vy Ngo Chi <ncv.ngovy@gmail.com>
2026-07-09 09:32:20 +0700

    chore: remove temporary migration scripts

--- apps/sale/next.config.ts
+++ apps/sale/next.config.ts
@@ -1,18 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ['@workspace/ui', '@workspace/core'],
+  transpilePackages: ["@workspace/ui", "@workspace/core"],
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "picsum.photos",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
     ],
   },
 };
 
 export default nextConfig;
-
-
-
