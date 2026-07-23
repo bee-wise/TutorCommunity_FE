32ca68c9924bfbe09bf9bf908578798bef749bbe
Vy Ngo Chi <ncv.ngovy@gmail.com>
2026-07-23 12:45:48 +0700

    feat: implement authentication logic including login, logout, role-based redirection, and a unified auth layout component

--- packages/core/hooks/useLogout.ts
+++ packages/core/hooks/useLogout.ts
@@ -1,20 +1,17 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { useRouter } from "next/navigation";
 import { authService } from "../services/auth.service";
 import { useAuthStore } from "../store/useAuthStore";
 
 export const useLogout = () => {
   const logout = useAuthStore((s) => s.logout);
   const queryClient = useQueryClient();
-  const router = useRouter();
 
   return useMutation({
     mutationFn: () => authService.logout(),
     onSettled: () => {
       logout();
       queryClient.clear();
-      router.push("/");
-      router.refresh();
+      window.location.href = "/";
     },
   });
 };
